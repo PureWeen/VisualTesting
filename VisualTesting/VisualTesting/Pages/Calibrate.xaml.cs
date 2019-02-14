cb3bf8c7a98855b31938f00c029ac1ecd2320985
Shane <shane94@hotmail.com>
2019-02-14 15:52:07 -0500

    add scrollviews

---
+++ VisualTesting/VisualTesting/Pages/Calibrate.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace VisualTesting.Pages
+{
+    public partial class Calibrate : ContentPage
+    {
+        public Calibrate()
+        {
+            InitializeComponent();
+        }
+    }
+}
