8576af8db80c0e877238f55b30c83a62b364304d
ShaneN <shane94@hotmail.com>
2019-02-26 23:07:08 -0700

    custom renderers

---
+++ VisualTesting/VisualTesting.Android/CustomVisualRenderer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using VisualTesting.Droid;
+using Xamarin.Forms;
+using Xamarin.Forms.Material.Android;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(Xamarin.Forms.Button), typeof(CustomVisualRenderer), new[] { typeof(VisualTesting.CustomVisual) })]
+namespace VisualTesting.Droid
+{
+	public class CustomVisualRenderer : MaterialButtonRenderer
+	{
+		public CustomVisualRenderer(Context context) : base(context)
+		{
+		}
+
+		protected override void OnElementChanged(ElementChangedEventArgs<Xamarin.Forms.Button> e)
+		{
+			base.OnElementChanged(e);
+			Element.Text = "I am a Custom Visual Renderer";
+		}
+	}
+}
---
+++ VisualTesting/VisualTesting.iOS/CustomVisualRenderer.cs
@@ -0,0 +1,21 @@
+﻿
+using System.ComponentModel;
+
+using VisualTesting.iOS;
+using Xamarin.Forms;
+using Xamarin.Forms.Material.iOS;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(Xamarin.Forms.Button), typeof(CustomVisualRenderer), new[] { typeof(VisualTesting.CustomVisual) })]
+namespace VisualTesting.iOS
+{
+	public class CustomVisualRenderer : MaterialButtonRenderer
+	{
+
+		protected override void OnElementChanged(ElementChangedEventArgs<Xamarin.Forms.Button> e)
+		{
+			base.OnElementChanged(e);
+			Element.Text = "I am a Custom Visual Renderer";
+		}
+	}
+}
